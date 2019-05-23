78ba669524c516c739c0120c7ae0a377679ac0e9
Phil Nicholson <nicholsonpk@gmail.com>
2019-05-23 15:32:35 -0400

    Player class added. Handles jumps. Many locations adjusted

--- Game.gd
+++ Game.gd
@@ -1,20 +1,98 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var whereIsPlayer
+
+var PLAYER_CAM_SPEED = 2000
+
+var CAM_MAXX = 4815
+var CAM_MINX = 3
+var CAM_MAXY = 3
+var CAM_MINY = -4809
+
+var PLAY_MAXX = 5530
+var PLAY_MINX = 80
+var PLAY_MAXY = 715
+var PLAY_MINY = -4720
+
+enum runningOn { bottom, right, top, left }
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	whereIsPlayer = runningOn.bottom
+	get_node("Player").global_position.x = PLAY_MINX
+	get_node("Player").global_position.y = PLAY_MAXY
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		get_node("Player").global_position.x += 1000 * delta
-	if Input.is_action_pressed("ui_left"):
-		get_node("Player").global_position.x -= 1000 * delta
+	if (whereIsPlayer == runningOn.bottom):
+		
+		# Move player and camera
+		get_node("Player").global_position.x += PLAYER_CAM_SPEED * delta
+		get_node("Camera2D").global_position.x += PLAYER_CAM_SPEED * delta
+		
+		# Limit camera movement 
+		if (get_node("Camera2D").global_position.x > CAM_MAXX):
+			get_node("Camera2D").global_position.x = CAM_MAXX
+		
+		# Player at edge, rotate
+		if (get_node("Player").global_position.x >= PLAY_MAXX):
+			whereIsPlayer = runningOn.right
+			get_node("Player").setWhereIsPlayer(runningOn.right)
+			get_node("Player").rotate(-1.570796)
 	
-	if (get_node("Player").global_position.x >= 500):
-		#begin tilt until 5600
-		get_node("Player").rotate(-((9/20) * (700 - get_node("Player").global_position.x)))
+	if (whereIsPlayer == runningOn.right):
+		
+		# Move player and camera
+		get_node("Player").global_position.y -= PLAYER_CAM_SPEED * delta
+		get_node("Camera2D").global_position.y -= PLAYER_CAM_SPEED * delta
+		
+		# Limit camera movement 
+		if (get_node("Camera2D").global_position.y < CAM_MINY):
+			get_node("Camera2D").global_position.y = CAM_MINY
+		
+		# Player at edge, rotate
+		if (get_node("Player").global_position.y <= PLAY_MINY):
+			whereIsPlayer = runningOn.top
+			get_node("Player").setWhereIsPlayer(runningOn.top)
+			get_node("Player").rotate(-1.570796)
+		
+		
+	if (whereIsPlayer == runningOn.top):
+		
+		# Move player and camera
+		get_node("Player").global_position.x -= PLAYER_CAM_SPEED * delta
+		get_node("Camera2D").global_position.x -= PLAYER_CAM_SPEED * delta
+		
+		# Limit camera movement 
+		if (get_node("Camera2D").global_position.x < CAM_MINX):
+			get_node("Camera2D").global_position.x = CAM_MINX
+		
+		# Player at edge, rotate
+		if (get_node("Player").global_position.x <= PLAY_MINX):
+			whereIsPlayer = runningOn.left
+			get_node("Player").setWhereIsPlayer(runningOn.left)
+			get_node("Player").rotate(-1.570796)
+		
+		
+		
+	if (whereIsPlayer == runningOn.left):
+		
+		# Move player and camera
+		get_node("Player").global_position.y += PLAYER_CAM_SPEED * delta
+		get_node("Camera2D").global_position.y += PLAYER_CAM_SPEED * delta
+		
+		# Limit camera movement 
+		if (get_node("Camera2D").global_position.y > CAM_MAXY):
+			get_node("Camera2D").global_position.y = CAM_MAXY
+		
+		# Player at edge, rotate
+		if (get_node("Player").global_position.y >= PLAY_MAXY):
+			whereIsPlayer = runningOn.bottom
+			get_node("Player").setWhereIsPlayer(runningOn.bottom)
+			get_node("Player").rotate(-1.570796)
+
+
+func getDirection():
+	return whereIsPlayer
---
+++ Player.gd
@@ -0,0 +1,115 @@
+extends Node2D
+
+var isJumping = false
+var finishedJump = false
+var whereIsPlayer
+var JUMP_SPEED = 300
+var JUMP_HEIGHT = 100
+
+var image1 = preload("res://images/player_anim/Sprite-0001.png")
+var image2 = preload("res://images/player_anim/Sprite-0002.png")
+var image3 = preload("res://images/player_anim/Sprite-0003.png")
+var image4 = preload("res://images/player_anim/Sprite-0004.png")
+var currentFrame = 1
+var timeSinceFrame = 0
+
+enum runningOn { bottom, right, top, left }
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node(".").set_texture(image1)
+	whereIsPlayer = runningOn.bottom
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	timeSinceFrame += delta
+	if (timeSinceFrame >= .1):
+		currentFrame += 1
+		
+		if (currentFrame == 1):
+			get_node(".").set_texture(image1)
+		if (currentFrame == 2):
+			get_node(".").set_texture(image2)
+		if (currentFrame == 3):
+			get_node(".").set_texture(image3)
+		if (currentFrame == 4):
+			get_node(".").set_texture(image4)
+			currentFrame = 1
+		
+		timeSinceFrame = 0
+	
+	if (isJumping == false && Input.is_action_pressed("ui_up")):
+		# jumping
+		isJumping = true
+
+	if (isJumping):
+		
+		# bottom
+		if (whereIsPlayer == runningOn.bottom):
+			
+			# move upward
+			if (!finishedJump):
+				get_node(".").global_position.y -= JUMP_SPEED * delta
+				if (get_node(".").global_position.y <= 715 - JUMP_HEIGHT):
+					finishedJump = true
+			
+			# drop down
+			if (finishedJump):
+				if (get_node(".").global_position.y <= 715):
+					get_node(".").global_position.y += JUMP_SPEED * 2 * delta # fall twice as fast
+				else:
+					isJumping = false
+					finishedJump = false
+		
+		# right
+		if (whereIsPlayer == runningOn.right):
+			
+			if (!finishedJump):
+				get_node(".").global_position.x -= JUMP_SPEED * delta
+				if (get_node(".").global_position.x <= 5530 - JUMP_HEIGHT):
+					finishedJump = true
+			
+			if (finishedJump):
+				if (get_node(".").global_position.x <= 5530):
+					get_node(".").global_position.x += JUMP_SPEED * 2 * delta # fall twice as fast
+				else:
+					isJumping = false
+					finishedJump = false
+		
+		# top
+		if (whereIsPlayer == runningOn.top):
+			
+			# move upward
+			if (!finishedJump):
+				get_node(".").global_position.y += JUMP_SPEED * delta
+				if (get_node(".").global_position.y >= -4720 + JUMP_HEIGHT):
+					finishedJump = true
+			
+			# drop down
+			if (finishedJump):
+				if (get_node(".").global_position.y >= -4720):
+					get_node(".").global_position.y -= JUMP_SPEED * 2 * delta # fall twice as fast
+				else:
+					isJumping = false
+					finishedJump = false
+		
+		# left
+		if (whereIsPlayer == runningOn.left):
+			
+			# move upward
+			if (!finishedJump):
+				get_node(".").global_position.x += JUMP_SPEED * delta
+				if (get_node(".").global_position.x >= 83 + JUMP_HEIGHT):
+					finishedJump = true
+			
+			# drop down
+			if (finishedJump):
+				if (get_node(".").global_position.x >= 83):
+					get_node(".").global_position.x -= JUMP_SPEED * 2 * delta # fall twice as fast
+				else:
+					isJumping = false
+					finishedJump = false
+
+func setWhereIsPlayer(var x):
+	whereIsPlayer = x
