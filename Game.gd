c727e143e09b21e8a6c763b98aa925285eebc9aa
Phil Nicholson <nicholsonpk@gmail.com>
2019-05-26 13:21:37 -0400

    Hard coded checks dropped in favor of move_and_slide()

--- Game.gd
+++ Game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var whereIsPlayer
 
-var PLAYER_CAM_SPEED = 1500
+var PLAYER_CAM_SPEED = 350
 
 var CAM_MAXX = 4815
 var CAM_MINX = 3
@@ -24,78 +24,78 @@ func _ready():
 	get_node("KinematicBody2D/Player").global_position.y = PLAY_MAXY
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if (whereIsPlayer == runningOn.bottom):
-		
-		# Move player and camera
-		get_node("KinematicBody2D/Player").global_position.x += PLAYER_CAM_SPEED * delta
-		get_node("Camera2D").global_position.x += PLAYER_CAM_SPEED * delta
-		
-		# Limit camera movement 
-		if (get_node("Camera2D").global_position.x > CAM_MAXX):
-			get_node("Camera2D").global_position.x = CAM_MAXX
-		
-		# Player at edge, rotate
-		if (get_node("KinematicBody2D/Player").global_position.x >= PLAY_MAXX):
-			whereIsPlayer = runningOn.right
-			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.right)
-			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
-			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
-	
-	if (whereIsPlayer == runningOn.right):
-		
-		# Move player and camera
-		get_node("KinematicBody2D/Player").global_position.y -= PLAYER_CAM_SPEED * delta
-		get_node("Camera2D").global_position.y -= PLAYER_CAM_SPEED * delta
-		
-		# Limit camera movement 
-		if (get_node("Camera2D").global_position.y < CAM_MINY):
-			get_node("Camera2D").global_position.y = CAM_MINY
-		
-		# Player at edge, rotate
-		if (get_node("KinematicBody2D/Player").global_position.y <= PLAY_MINY):
-			whereIsPlayer = runningOn.top
-			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.top)
-			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
-			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
-		
-		
-	if (whereIsPlayer == runningOn.top):
-		
-		# Move player and camera
-		get_node("KinematicBody2D/Player").global_position.x -= PLAYER_CAM_SPEED * delta
-		get_node("Camera2D").global_position.x -= PLAYER_CAM_SPEED * delta
-		
-		# Limit camera movement 
-		if (get_node("Camera2D").global_position.x < CAM_MINX):
-			get_node("Camera2D").global_position.x = CAM_MINX
-		
-		# Player at edge, rotate
-		if (get_node("KinematicBody2D/Player").global_position.x <= PLAY_MINX):
-			whereIsPlayer = runningOn.left
-			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.left)
-			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
-			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
-		
-		
-		
-	if (whereIsPlayer == runningOn.left):
-		
-		# Move player and camera
-		get_node("KinematicBody2D/Player").global_position.y += PLAYER_CAM_SPEED * delta
-		get_node("Camera2D").global_position.y += PLAYER_CAM_SPEED * delta
-		
-		# Limit camera movement 
-		if (get_node("Camera2D").global_position.y > CAM_MAXY):
-			get_node("Camera2D").global_position.y = CAM_MAXY
-		
-		# Player at edge, rotate
-		if (get_node("KinematicBody2D/Player").global_position.y >= PLAY_MAXY):
-			whereIsPlayer = runningOn.bottom
-			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.bottom)
-			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
-			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	if (whereIsPlayer == runningOn.bottom):
+#
+#		# Move player and camera
+#		get_node("KinematicBody2D/Player").global_position.x += PLAYER_CAM_SPEED * delta
+#		get_node("Camera2D").global_position.x += PLAYER_CAM_SPEED * delta
+#
+#		# Limit camera movement 
+#		if (get_node("Camera2D").global_position.x > CAM_MAXX):
+#			get_node("Camera2D").global_position.x = CAM_MAXX
+#
+#		# Player at edge, rotate
+#		if (get_node("KinematicBody2D/Player").global_position.x >= PLAY_MAXX):
+#			whereIsPlayer = runningOn.right
+#			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.right)
+#			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
+#			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
+#
+#	if (whereIsPlayer == runningOn.right):
+#
+#		# Move player and camera
+#		get_node("KinematicBody2D/Player").global_position.y -= PLAYER_CAM_SPEED * delta
+#		get_node("Camera2D").global_position.y -= PLAYER_CAM_SPEED * delta
+#
+#		# Limit camera movement 
+#		if (get_node("Camera2D").global_position.y < CAM_MINY):
+#			get_node("Camera2D").global_position.y = CAM_MINY
+#
+#		# Player at edge, rotate
+#		if (get_node("KinematicBody2D/Player").global_position.y <= PLAY_MINY):
+#			whereIsPlayer = runningOn.top
+#			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.top)
+#			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
+#			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
+#
+#
+#	if (whereIsPlayer == runningOn.top):
+#
+#		# Move player and camera
+#		get_node("KinematicBody2D/Player").global_position.x -= PLAYER_CAM_SPEED * delta
+#		get_node("Camera2D").global_position.x -= PLAYER_CAM_SPEED * delta
+#
+#		# Limit camera movement 
+#		if (get_node("Camera2D").global_position.x < CAM_MINX):
+#			get_node("Camera2D").global_position.x = CAM_MINX
+#
+#		# Player at edge, rotate
+#		if (get_node("KinematicBody2D/Player").global_position.x <= PLAY_MINX):
+#			whereIsPlayer = runningOn.left
+#			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.left)
+#			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
+#			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
+#
+#
+#
+#	if (whereIsPlayer == runningOn.left):
+#
+#		# Move player and camera
+#		get_node("KinematicBody2D/Player").global_position.y += PLAYER_CAM_SPEED * delta
+#		get_node("Camera2D").global_position.y += PLAYER_CAM_SPEED * delta
+#
+#		# Limit camera movement 
+#		if (get_node("Camera2D").global_position.y > CAM_MAXY):
+#			get_node("Camera2D").global_position.y = CAM_MAXY
+#
+#		# Player at edge, rotate
+#		if (get_node("KinematicBody2D/Player").global_position.y >= PLAY_MAXY):
+#			whereIsPlayer = runningOn.bottom
+#			get_node("KinematicBody2D").setWhereIsPlayer(runningOn.bottom)
+#			get_node("KinematicBody2D/Player").rotate(-1.570796) # 90 degrees to Rads
+#			get_node("KinematicBody2D/CollisionShape2D").rotate(-1.570796)
 
 
 func getDirection():
--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 
 var isJumping = false
-var finishedJump = false
 
 var isSliding = false
 var isRolling = false
@@ -11,8 +10,11 @@ var isRunning = true
 
 var whereIsPlayer
 
-var JUMP_SPEED = 600
-var JUMP_HEIGHT = 200
+var JUMP_SPEED = -1200
+var RUN_SPEED = 350
+var GRAVITY = 2500
+
+var velocity = Vector2()
 
 var runningImage1 = preload("res://images/player_anim/Sprite-0001.png")
 var runningImage2 = preload("res://images/player_anim/Sprite-0002.png")
@@ -32,6 +34,32 @@ enum runningOn { bottom, right, top, left }
 func _ready():
 	get_node("Player").set_texture(runningImage1)
 	whereIsPlayer = runningOn.bottom
+
+func get_input():
+	velocity.x = 0
+	var right = RUN_SPEED
+	#var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_up')
+	
+	if (Input.is_action_just_pressed('ui_down')):
+		isSliding = true
+		isJumping = false
+		isRunning = false
+	
+	if is_on_floor() and jump:
+		isJumping = true
+		isRunning = false
+		velocity.y = JUMP_SPEED
+	if is_on_floor() and !jump:
+		isRunning = true
+		isJumping = false
+	if right:
+		velocity.x += RUN_SPEED
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -59,8 +87,8 @@ func _process(delta):
 				currentFrame = 1
 
 		# Jumping
-#		if (isJumping):
-#			get_node("Player").set_texture(jumpingImage1)
+		if (isJumping):
+			get_node("Player").set_texture(rollingImage1)
 
 		# Sliding
 		if (isSliding && !isRolling):
@@ -72,137 +100,137 @@ func _process(delta):
 
 
 		timeSinceFrame = 0
-
-
-	#
-	#
-	# Check inputs
-	#
-	#
-
-	# Neither input pressed
-	if (!Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down") && !isJumping):
-		isRunning = true
-
-	# Up input pressed
-	if (isJumping == false  && isSliding == false && isRolling ==  false && Input.is_action_pressed("ui_up")):
-		# jumping
-		isJumping = true
-
-	# Down input pressed
-	if (isJumping == false && isSliding == false && isRolling ==  false && Input.is_action_pressed("ui_down")):
-		# sliding
-		isSliding = true
-
-	#
-	# isRolling is set inside isSliding code
-	#
-
-	#
-	#
-	# Jumping
-	#
-	#
-
-	if (isJumping):
-
-		# bottom
-		if (whereIsPlayer == runningOn.bottom):
-
-			# move upward
-			if (!finishedJump):
-				get_node("Player").global_position.y -= JUMP_SPEED * delta
-				if (get_node("Player").global_position.y <= 715 - JUMP_HEIGHT):
-					finishedJump = true
-
-			# drop down
-			if (finishedJump):
-				if (get_node("Player").global_position.y <= 715):
-					get_node("Player").global_position.y += JUMP_SPEED * 2 * delta # fall twice as fast
-				else:
-					isJumping = false
-					finishedJump = false
-
-		# right
-		if (whereIsPlayer == runningOn.right):
-
-			if (!finishedJump):
-				get_node("Player").global_position.x -= JUMP_SPEED * delta
-				if (get_node("Player").global_position.x <= 5530 - JUMP_HEIGHT):
-					finishedJump = true
-
-			if (finishedJump):
-				if (get_node("Player").global_position.x <= 5530):
-					get_node("Player").global_position.x += JUMP_SPEED * 2 * delta # fall twice as fast
-				else:
-					isJumping = false
-					finishedJump = false
-
-		# top
-		if (whereIsPlayer == runningOn.top):
-
-			# move upward
-			if (!finishedJump):
-				get_node("Player").global_position.y += JUMP_SPEED * delta
-				if (get_node("Player").global_position.y >= -4720 + JUMP_HEIGHT):
-					finishedJump = true
-
-			# drop down
-			if (finishedJump):
-				if (get_node("Player").global_position.y >= -4720):
-					get_node("Player").global_position.y -= JUMP_SPEED * 2 * delta # fall twice as fast
-				else:
-					isJumping = false
-					finishedJump = false
-
-		# left
-		if (whereIsPlayer == runningOn.left):
-
-			# move upward
-			if (!finishedJump):
-				get_node("Player").global_position.x += JUMP_SPEED * delta
-				if (get_node("Player").global_position.x >= 83 + JUMP_HEIGHT):
-					finishedJump = true
-
-			# drop down
-			if (finishedJump):
-				if (get_node("Player").global_position.x >= 83):
-					get_node("Player").global_position.x -= JUMP_SPEED * 2 * delta # fall twice as fast
-				else:
-					isJumping = false
-					finishedJump = false
-
-	#
-	#
-	# Sliding and Rolling
-	#
-	#
-	if (isSliding):
-
-		# if the button is held down,
-		# count length of time
-		if (Input.is_action_pressed("ui_down")):
-			isSliding = true
-			slideRollTimer += delta
-		else: # otherwise, reset timer and bools
-			slideRollTimer = 0
-			isSliding = false
-			isRolling = false
-
-		if (slideRollTimer > .5):
-			# Rolling
-			isRolling = true
-
-#	# print to output for testing
-#	var run
-#	var jump
-#	var slide
-#	var roll
-#	run = "true" if isRunning else "false"
-#	jump = "true" if isJumping else "false"
-#	slide = "true" if isSliding else "false"
-#	roll = "true" if isRolling else "false"
-#	print(run + jump + slide + roll)
+#
+#
+#	#
+#	#
+#	# Check inputs
+#	#
+#	#
+#
+#	# Neither input pressed
+#	if (!Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down") && !isJumping):
+#		isRunning = true
+#
+#	# Up input pressed
+#	if (isJumping == false  && isSliding == false && isRolling ==  false && Input.is_action_pressed("ui_up")):
+#		# jumping
+#		isJumping = true
+#
+#	# Down input pressed
+#	if (isJumping == false && isSliding == false && isRolling ==  false && Input.is_action_pressed("ui_down")):
+#		# sliding
+#		isSliding = true
+#
+#	#
+#	# isRolling is set inside isSliding code
+#	#
+#
+#	#
+#	#
+#	# Jumping
+#	#
+#	#
+#
+#	if (isJumping):
+#
+#		# bottom
+#		if (whereIsPlayer == runningOn.bottom):
+#
+#			# move upward
+#			if (!finishedJump):
+#				get_node("Player").global_position.y -= JUMP_SPEED * delta
+#				if (get_node("Player").global_position.y <= 715 - JUMP_HEIGHT):
+#					finishedJump = true
+#
+#			# drop down
+#			if (finishedJump):
+#				if (get_node("Player").global_position.y <= 715):
+#					get_node("Player").global_position.y += JUMP_SPEED * 2 * delta # fall twice as fast
+#				else:
+#					isJumping = false
+#					finishedJump = false
+#
+#		# right
+#		if (whereIsPlayer == runningOn.right):
+#
+#			if (!finishedJump):
+#				get_node("Player").global_position.x -= JUMP_SPEED * delta
+#				if (get_node("Player").global_position.x <= 5530 - JUMP_HEIGHT):
+#					finishedJump = true
+#
+#			if (finishedJump):
+#				if (get_node("Player").global_position.x <= 5530):
+#					get_node("Player").global_position.x += JUMP_SPEED * 2 * delta # fall twice as fast
+#				else:
+#					isJumping = false
+#					finishedJump = false
+#
+#		# top
+#		if (whereIsPlayer == runningOn.top):
+#
+#			# move upward
+#			if (!finishedJump):
+#				get_node("Player").global_position.y += JUMP_SPEED * delta
+#				if (get_node("Player").global_position.y >= -4720 + JUMP_HEIGHT):
+#					finishedJump = true
+#
+#			# drop down
+#			if (finishedJump):
+#				if (get_node("Player").global_position.y >= -4720):
+#					get_node("Player").global_position.y -= JUMP_SPEED * 2 * delta # fall twice as fast
+#				else:
+#					isJumping = false
+#					finishedJump = false
+#
+#		# left
+#		if (whereIsPlayer == runningOn.left):
+#
+#			# move upward
+#			if (!finishedJump):
+#				get_node("Player").global_position.x += JUMP_SPEED * delta
+#				if (get_node("Player").global_position.x >= 83 + JUMP_HEIGHT):
+#					finishedJump = true
+#
+#			# drop down
+#			if (finishedJump):
+#				if (get_node("Player").global_position.x >= 83):
+#					get_node("Player").global_position.x -= JUMP_SPEED * 2 * delta # fall twice as fast
+#				else:
+#					isJumping = false
+#					finishedJump = false
+#
+#	#
+#	#
+#	# Sliding and Rolling
+#	#
+#	#
+#	if (isSliding):
+#
+#		# if the button is held down,
+#		# count length of time
+#		if (Input.is_action_pressed("ui_down")):
+#			isSliding = true
+#			slideRollTimer += delta
+#		else: # otherwise, reset timer and bools
+#			slideRollTimer = 0
+#			isSliding = false
+#			isRolling = false
+#
+#		if (slideRollTimer > .5):
+#			# Rolling
+#			isRolling = true
+#
+##	# print to output for testing
+##	var run
+##	var jump
+##	var slide
+##	var roll
+##	run = "true" if isRunning else "false"
+##	jump = "true" if isJumping else "false"
+##	slide = "true" if isSliding else "false"
+##	roll = "true" if isRolling else "false"
+##	print(run + jump + slide + roll)
 
 #
 #
